e91b88286cec4891ca056e270d752e2a49d197ae
DENNER Charles <>
2024-11-23 12:42:53 +0100

    add karma

---
+++ karma.conf.js
@@ -0,0 +1,34 @@
+module.exports = function (config) {
+    config.set({
+      frameworks: ['jasmine', '@angular-devkit/build-angular'],
+      plugins: [
+        require('karma-jasmine'),
+        require('karma-chrome-launcher'),
+        require('karma-jasmine-html-reporter'),
+        require('karma-coverage'),
+        require('@angular-devkit/build-angular/plugins/karma')
+      ],
+      client: {
+        clearContext: false // laisse Jasmine afficher le rapport complet
+      },
+      reporters: ['progress', 'coverage'],
+      coverageReporter: {
+        dir: require('path').join(__dirname, './coverage'),
+        subdir: '.',
+        reporters: [{ type: 'html' }, { type: 'text-summary' }]
+      },
+      port: 9876,
+      colors: true,
+      logLevel: config.LOG_INFO,
+      autoWatch: false,
+      browsers: ['ChromeHeadless'],
+      customLaunchers: {
+        ChromeHeadless: {
+          base: 'ChromeHeadless',
+          flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage']
+        }
+      },
+      singleRun: true // s'assure que Karma quitte après les tests
+    });
+  };
+  
